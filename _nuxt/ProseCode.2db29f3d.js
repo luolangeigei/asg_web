3dfec11c532e053a261b8494e7e690eca4f31efb
chufeng-official <1148006460@qq.com>
2023-07-30 21:49:08 +0800

    修复了在战队投票页面修改页面尺寸，使宽窄状态修改后，队伍数据会重新获取并造成冗余的错误

---
+++ _nuxt/ProseCode.2db29f3d.js
@@ -0,0 +1,1 @@
+import{_ as t,f as n,a3 as a}from"./entry.b4c3248d.js";const r=n({props:{code:{type:String,default:""},language:{type:String,default:null},filename:{type:String,default:null},highlights:{type:Array,default:()=>[]},meta:{type:String,default:null}}});function o(e,l,s,d,p,u){return a(e.$slots,"default")}const _=t(r,[["render",o]]);export{_ as default};
---
+++ _nuxt/ProseTh.cb287543.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,c as t,a3 as s}from"./entry.b4c3248d.js";const c={};function n(e,a){return r(),t("th",null,[s(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
